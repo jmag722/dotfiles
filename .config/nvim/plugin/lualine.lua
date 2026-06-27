ad8eb45447fcdf047dfc012870f1239b15f48f24
Jared Magnusson <jmag722@gmail.com>
2026-06-27 09:10:17 -0500

    add lualine support --- Signed-off-by: Jared Magnusson <jmag722@gmail.com>

---
+++ .config/nvim/plugin/lualine.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+  'https://github.com/nvim-lualine/lualine.nvim'
+})
+
+-- Initialize and configure lualine
+require('lualine').setup({
+})
--- .config/nvim/plugin/mini.icons.lua
+++ .config/nvim/plugin/mini.icons.lua
@@ -2,3 +2,4 @@ vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.icons', version = 'stable' },
 })
 require('mini.icons').setup()
+require('mini.icons').mock_nvim_web_devicons()
